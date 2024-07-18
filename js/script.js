92b7a52f8cc7f3086426935df9f852df95af7c5a
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-07-17 21:45:14 -0500

    modo oscuro nav y body

--- js/script.js
+++ js/script.js
@@ -36,3 +36,18 @@ document.addEventListener("click", (event) => {
         document.body.classList.remove("no-scroll");
     }
 });
+
+//Modo Oscuro
+let toggle=document.getElementById('toggle');
+let label_toogle=document.getElementById('label_toggle');
+toggle.addEventListener('change', (event)=>{
+    let checked=event.target.checked;
+    document.body.classList.toggle('dark');
+    if(checked == true){
+        label_toogle.innerHTML="<i class='bx bxs-sun' ></i>";
+        label_toogle.style.color="var(--DarkHeaderBackground)";
+    }else{
+        label_toogle.innerHTML="<i class='bx bxs-moon'></i>"; 
+        label_toogle.style.color="var(--ColorBlanco)";
+    }
+});
